70b24709afc5e016cc1612672dd7ddc648f19347
Kisung Shin <ksshin@gpu-1.novalocal>
2024-11-12 20:04:22 +0900

    reduce training time to 11hours for 2000 epoch

--- utils.py
+++ utils.py
@@ -3,82 +3,98 @@ import torch
 import os
 import h5py
 from torch.utils.data import TensorDataset, DataLoader
+import time
+from tqdm import tqdm
 
 import IPython
 e = IPython.embed
 
 class EpisodicDataset(torch.utils.data.Dataset):
     def __init__(self, episode_ids, dataset_dir, camera_names, norm_stats):
-        super(EpisodicDataset).__init__()
+        super(EpisodicDataset, self).__init__()
         self.episode_ids = episode_ids
         self.dataset_dir = dataset_dir
         self.camera_names = camera_names
         self.norm_stats = norm_stats
         self.is_sim = None
-        self.__getitem__(0) # initialize self.is_sim
+        self.file_cache = {}  # HDF5 파일 캐시 추가
+        self.cache_size = 10000000  # 최대 캐시 크기 설정
+        
+        print("EPISODE IDS", episode_ids)
+        print("FIRST PUT ALL FILES INTO MEMORY")
+        for episode_idx in tqdm(episode_ids):
+            dataset_path = os.path.join(self.dataset_dir, f'traj_{episode_idx}.h5')
+            if len(self.file_cache) >= self.cache_size:
+                # 캐시에서 가장 오래된 파일 제거 및 닫기
+                old_path, old_file = self.file_cache.popitem()
+                old_file.close()
+            file = h5py.File(dataset_path, 'r')[f'/dict_str_traj_{episode_idx}/dict_str_obs']
+            root = {'state': file['dict_str_state'][:], 'rgb': file['dict_str_rgb'][:]}
+            self.file_cache[dataset_path] = root
+        
+        self.__getitem__(0)  # self.is_sim 초기화
+        
 
     def __len__(self):
         return len(self.episode_ids)
 
     def __getitem__(self, index):
-        sample_full_episode = False # hardcode
+        sample_full_episode = False  # 하드코딩
         episode_idx = self.episode_ids[index]
-        dataset_path = os.path.join(self.dataset_dir, f'traj_{episode_idx}.h5')#f'episode_{episode_id}.hdf5')
-        with h5py.File(dataset_path, 'r') as root:
-            is_sim = False #root.attrs['sim']
-            original_action_shape = root[f'/dict_str_traj_{episode_idx}/dict_str_obs/dict_str_state'].shape#root['/action'].shape
-            episode_len = original_action_shape[0]
-            if sample_full_episode:
-                start_ts = 0
-            else:
-                start_ts = np.random.choice(episode_len)
-            # get observation at start_ts only
-            qpos = root[f'/dict_str_traj_{episode_idx}/dict_str_obs/dict_str_state'][start_ts]#root['/observations/qpos'][start_ts]
-            qvel = root[f'/dict_str_traj_{episode_idx}/dict_str_obs/dict_str_state'][start_ts]#root['/observations/qvel'][start_ts]# not to use learning but just write
-            image_dict = dict()
-            assert root[f'/dict_str_traj_{episode_idx}/dict_str_obs/dict_str_rgb'].shape[1] == len(self.camera_names), f"camera num different: {self.camera_names}"
-            for i, cam_name in enumerate(self.camera_names):
-                image_dict[cam_name] = np.transpose(root[f'/dict_str_traj_{episode_idx}/dict_str_obs/dict_str_rgb'][start_ts][i], (1, 2, 0))#root[f'/observations/images/{cam_name}'][start_ts]
-                
-            # get all actions after and including start_ts
-            if is_sim:
-                action = root[f'/dict_str_traj_{episode_idx}/dict_str_obs/dict_str_state'][start_ts:]#root['/action'][start_ts:]
-                action_len = episode_len - start_ts
-            else:
-                start = max(0, start_ts - 1)
-                end = start + 150
-                action = root[f'/dict_str_traj_{episode_idx}/dict_str_obs/dict_str_state'][start: end]#root['/action'][max(0, start_ts - 1):] # hack, to make timesteps more aligned
-                action_len = 150#episode_len - max(0, start_ts - 1) # hack, to make timesteps more aligned
+        dataset_path = os.path.join(self.dataset_dir, f'traj_{episode_idx}.h5')
+        
+        root = self.file_cache[dataset_path]
+        # 데이터 로딩 및 처리
+        is_sim = False  # root.attrs['sim']
+        original_action_shape = root['state'].shape
+        episode_len = original_action_shape[0]
+
+        if sample_full_episode:
+            start_ts = 0
+        else:
+            start_ts = np.random.choice(episode_len)
+        # 시작 시간의 관찰 값 가져오기
+        qpos = root['state'][start_ts]
+        qvel = root['state'][start_ts]
+        image_dict = {}
+        assert root['rgb'].shape[1] == len(self.camera_names), f"카메라 수가 다릅니다: {self.camera_names}"
+        images = root['rgb'][start_ts]
+        for i, cam_name in enumerate(self.camera_names):
+            image = images[i]
+            image_dict[cam_name] = np.transpose(image, (1, 2, 0))
+        # 시작 시간 이후의 모든 액션 가져오기
+        if is_sim:
+            action = root['state'][start_ts:]
+            action_len = episode_len - start_ts
+        else:
+            start = max(0, start_ts - 1)
+            end = start + 150
+            action = root['state'][start:end]
+            action_len = 150
 
         self.is_sim = is_sim
-        padded_action = np.zeros((150, original_action_shape[1]), dtype=np.float32)#np.zeros(original_action_shape, dtype=np.float32)
-        padded_action[:len(action)] = action#[:action_len] = action
+        padded_action = np.zeros((150, original_action_shape[1]), dtype=np.float32)
+        padded_action[:len(action)] = action
 
-        
-        is_pad = np.zeros(150)#np.zeros(episode_len)
-        is_pad[len(action):] = 1#[action_len:] = 1
+        is_pad = np.zeros(150)
+        is_pad[len(action):] = 1
 
-        # new axis for different cameras
-        all_cam_images = []
-        for cam_name in self.camera_names:
-            all_cam_images.append(image_dict[cam_name])
+        # 여러 카메라의 이미지를 스택
+        all_cam_images = [image_dict[cam_name] for cam_name in self.camera_names]
         all_cam_images = np.stack(all_cam_images, axis=0)
-
-        # construct observations
-        image_data = torch.from_numpy(all_cam_images)
+        # 텐서로 변환 및 전처리
+        image_data = torch.from_numpy(all_cam_images).float() / 255.0
         qpos_data = torch.from_numpy(qpos).float()
         action_data = torch.from_numpy(padded_action).float()
         is_pad = torch.from_numpy(is_pad).bool()
 
-        # channel last
-        image_data = torch.einsum('k h w c -> k c h w', image_data)
-
-        # normalize image and change dtype to float
-        image_data = image_data / 255.0
+        # 채널 순서 변경 (채널 마지막에서 첫 번째로)
+        image_data = image_data.permute(0, 3, 1, 2)
+        # 정규화
         action_data = (action_data - self.norm_stats["action_mean"]) / self.norm_stats["action_std"]
         qpos_data = (qpos_data - self.norm_stats["qpos_mean"]) / self.norm_stats["qpos_std"]
-
         return image_data.float(), qpos_data.float(), action_data.float(), is_pad
+    
 
 
 def get_norm_stats(dataset_dir, num_episodes):
