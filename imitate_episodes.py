9846bb870eefc4961cc29d008f113b13814def47
KisungShin-SNU <virtualkss@snu.ac.kr>
2025-03-21 21:11:23 +0900

    250321_seperate_setting_success

--- imitate_episodes.py
+++ imitate_episodes.py
@@ -116,7 +116,7 @@ def main(args):
 
                 ## 1119 세팅!! TODO 바꿔야함
                 reset_joints_left = np.deg2rad([-90, -90, -90, -90, 90, 0, 0])
-                reset_joints_right = np.deg2rad([90, -90, 90, -90, -90, 180, 0, 0])
+                reset_joints_right = np.deg2rad([90, -90, 90, -90, -90, 180, 0])
                 reset_joints = np.concatenate([reset_joints_left, reset_joints_right])
                 curr_joints = env.get_obs()["joint_positions"]
                 print('virtualkss_start:')
--- inference.sh
+++ inference.sh
@@ -14,15 +14,15 @@ SESSION_NAME="launch_nodes_acct"
 tmux new-session -d -s "$SESSION_NAME"
 
 # for BIMANUAL SETTING
-#tmux send-keys -t $SESSION_NAME:0 "bash -i -c 'conda activate gello && python experiments/launch_nodes.py --robot bimanual_ur'" C-m
+tmux send-keys -t $SESSION_NAME:0 "cd /home/bi_admin/Desktop/gello_software/ && bash -i -c 'conda activate gello && python experiments/launch_nodes.py --robot bimanual_ur'" C-m
 
 # for UNIMANUAL SETTING
 #tmux send-keys -t "$SESSION_NAME:0" "cd /home/biomen/projects/gello_software/ && bash -i -c 'conda activate gello && python experiments/launch_nodes.py --robot ur --robot_ip 192.168.0.4'" C-m
-tmux send-keys -t "$SESSION_NAME:0" "cd /home/biadmin/Desktop/gello_software/ && bash -i -c 'conda activate gello && python experiments/launch_nodes.py --robot bimanual_ur --robot_ip 192.168.0.44'" C-m
-tmux send-keys -t "$SESSION_NAME:0" "cd /home/biadmin/Desktop/gello_software/ && bash -i -c 'conda activate gello && python experiments/launch_nodes.py --robot bimanual_ur --robot_ip 192.168.0.43'" C-m
+#tmux send-keys -t "$SESSION_NAME:0" "cd /home/bi_admin/Desktop/gello_software/ && bash -i -c 'conda activate gello && python experiments/launch_nodes.py --robot bimanual_ur --robot_ip 192.168.0.44'" C-m
+#tmux send-keys -t "$SESSION_NAME:0" "cd /home/bi_admin/Desktop/gello_software/ && bash -i -c 'conda activate gello && python experiments/launch_nodes.py --robot bimanual_ur --robot_ip 192.168.0.43'" C-m
 
 tmux split-window -v -t "$SESSION_NAME:0"
-tmux send-keys -t "$SESSION_NAME:0.1" "bash -i -c 'conda activate aloha && python3 imitate_episodes.py --task_name ${TASK_NAME} \
+tmux send-keys -t "$SESSION_NAME:0.1" "bash -i -c 'conda activate aloha && python3 imitate_episodes_gello.py --task_name ${TASK_NAME} \
   --ckpt_dir ${CKPT_DIR} \
   --policy_class ACT --kl_weight 10 --chunk_size ${CHUNK_SIZE} --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
   --num_epochs 2000 --lr 1e-5 --eval --temporal_agg \
